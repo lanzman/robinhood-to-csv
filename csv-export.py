089e650e7958cc72cc61ee825420956d5f3b1bc6
Michael Lanza <30355551+lanzman@users.noreply.github.com>
2017-12-13 11:46:20 -0500

    Forking update
    Updates to use pandas and improvements for speed, scalability, and updates

--- csv-export.py
+++ csv-export.py
@@ -1,9 +1,27 @@
+#change current working directory
+import os
+
+#set folderlocation for files
+folderloc = ""
+
+#checks if user left folder location blank
+if folderloc == "":
+    
+    print('Please update the fileloc where the program is stored. Press enter to exit.')
+    input()
+    quit()
+
+#change location for saved files
+os.chdir(folderloc)
+
 from Robinhood import Robinhood
 from profit_extractor import profit_extractor
 import getpass
 import collections
 import argparse
 import ast
+import sys
+import pandas as pd
 
 logged_in = False
 
@@ -58,16 +76,33 @@ while logged_in != True:
 
 print("Pulling trades. Please wait...")
 
-fields = collections.defaultdict(dict)
+# fetch transfer history and related metadata from the Robinhood API
+transfers = robinhood.get_endpoint('ach_transfers')
+
+# do/while for pagination
+paginated = True
+page = 0
+TransferList = pd.DataFrame()
+while paginated:
+    
+    #create and append transferlist
+    TransferList = TransferList.append(pd.DataFrame.from_dict(transfers['results'], orient = 'columns'))
+    
+    # paginate
+    if transfers['next'] is not None:
+        page = page + 1
+        transfers = robinhood.get_custom_endpoint(str(transfers['next']))
+    else:
+        paginated = False
+
+#Filename for TransferList
+TransferFilename = 'TransferList.csv'
+
+#writes to csv
+TransferList.to_csv(TransferFilename)
+
 trade_count = 0
 queued_count = 0
-
-# fetch order history and related metadata from the Robinhood API
-orders = robinhood.get_endpoint('orders')
-
-# load a debug file
-# raw_json = open('debug.txt','rU').read()
-# orders = ast.literal_eval(raw_json)
 
 # store debug
 if args.debug:
@@ -79,23 +114,91 @@ if args.debug:
     except IOError:
         print('Oops.  Unable to write file to debug.txt')
 
+# fetch order history and related metadata from the Robinhood API
+orders = robinhood.get_endpoint('orders')
+
+#Filename for Stored and master file of all transactions 
+CleanedMasterTransactionFilename = 'CleanedMasterTransactionList.csv'
+
+#Filename for last transactions 
+LastTransactionFilename = 'LastTransaction.csv'
+
+#Filename for new file that will contain updates if any exist
+UpdateTransactionFilename = 'UpdateTransactionList.csv'
+
+#Reads in LastTransaction from Orders
+LastTransaction = pd.DataFrame.from_dict(orders['results'], orient = 'columns')[0:1] \
+                    .astype({'average_price':'float', 'cumulative_quantity':'float', 'fees':'float', 'price':'float' \
+                             , 'quantity':'float'}) \
+                    .drop(['cancel', 'executions', 'reject_reason', 'stop_price'], axis = 1)
+
+#Checks if MasterTransactionList exists. If not, UpdateTransactionList does not need to be created
+if os.path.isfile(CleanedMasterTransactionFilename):
+    
+    if LastTransaction.equals(pd.read_csv(LastTransactionFilename, index_col=0, nrows = 1)):
+        
+        #Checks if UpdateTransactionFilename exists.
+        if os.path.isfile(UpdateTransactionFilename):    
+            
+            #prompt user there were no updates, gives option to delete update file
+            print("No updates found, would you like to delete the update file? Enter y/n", end=' ')
+            try:
+                input = raw_input
+            except NameError:
+                pass
+            y_n = input()   
+            
+            #if user selects yes, UpdateTransactionFile will be deleted
+            if y_n == "y":
+                
+                #deletes the update file
+                os.remove(UpdateTransactionFilename)
+        else:
+            
+            #prompt user there were no updates
+            print("No updates found, press enter to exit.", end=' ')
+            try:
+                input = raw_input
+            except NameError:
+                pass
+            input()
+            
+        sys.exit()
+        
+    #reads in CleanedMasterTransactionList.csv file date which is the last filled transaction
+    LastTransactionDate = pd.read_csv(CleanedMasterTransactionFilename, index_col=0, nrows = 1, parse_dates = ['date']).date[-1]
+    
+    #Sets UpdateTransaction Indicator to False
+    UpdateTransaction = True
+    
+else:
+    
+    #Sets UpdateTransaction Indicator to False
+    UpdateTransaction = False
+
 # do/while for pagination
 paginated = True
 page = 0
+TransactionList = pd.DataFrame()
 while paginated:
-    for i, order in enumerate(orders['results']):
-        executions = order['executions']
-        instrument = robinhood.get_custom_endpoint(order['instrument'])
-        fields[i + (page * 100)]['symbol'] = instrument['symbol']
-        for key, value in enumerate(order):
-            if value != "executions":
-                fields[i + (page * 100)][value] = order[value]
-        if order['state'] == "filled":
-            trade_count += 1
-            for key, value in enumerate(executions[0]):
-                fields[i + (page * 100)][value] = executions[0][value]
-        elif order['state'] == "queued":
-            queued_count += 1
+    
+   
+    #creates the executions df by pulling out the executions column from the orders['results']
+    #query used to keep index in tact for merging later, only care about state == "filled"
+    #removes the empty list values so the dataframe can be merged into the TransactionList dataframe 
+    #empty list values are conncurent with cancelled orders
+    order = pd.DataFrame.from_dict(orders['results'], orient = 'columns').query('state == "filled"')
+    
+    #converts to a dataframe and renames id column
+    executions = pd.DataFrame([dict(x[0]) for x in order.executions]) \
+                                .rename(columns = {'id' : 'exec_id'}) \
+                               .set_index(order.index)
+    
+    #combines the orders and executions dataframes and concats to the TransactionList dataframe
+    TransactionList = pd.concat([TransactionList, order.drop(['price','quantity'], axis = 1) \
+                                .merge(executions, how = 'inner', left_index = True, right_index = True) \
+                                .drop('executions', axis = 1)], ignore_index = True)                                              
+    
     # paginate
     if orders['next'] is not None:
         page = page + 1
@@ -103,53 +206,74 @@ while paginated:
     else:
         paginated = False
 
-# for i in fields:
-# 	print fields[i]
-# 	print "-------"
+#convert date to datetime for sorting
+TransactionList.timestamp = pd.to_datetime(TransactionList.timestamp, format = '%Y-%m-%dT%H:%M:%S')
+
+#sort by date with most recent at the top
+TransactionList = TransactionList.sort_values('timestamp', axis = 0, ascending= True).reset_index(drop= True)
+
+#if UpdateTransaction is True, will select only new records
+if UpdateTransaction:
+        
+    #Finds the last transaction index location        
+    LastTransLoc = TransactionList[TransactionList.timestamp == LastTransactionDate].index[0]+1
+            
+    #resets the dataframe to only contain new info
+    TransactionList = TransactionList.iloc[LastTransLoc:]
+    
+    #set paginated to False to end While Loop
+    paginated = False
 
 # check we have trade data to export
-if trade_count > 0 or queded_count > 0:
+if trade_count > 0 or queued_count > 0:
     print("%d queued trade%s and %d executed trade%s found in your account." %
           (queued_count, "s" [queued_count == 1:], trade_count,
            "s" [trade_count == 1:]))
-    # print str(queued_count) + " queded trade(s) and " + str(trade_count) + " executed trade(s) found in your account."
+  
+#gets symbol information
+TransactionList['symbol'] = [robinhood.get_custom_endpoint(x)['symbol'] for x in TransactionList['instrument']]
+     
+#removes unncessary columns, only looks at filled transactions, and renames fees column to commission
+TransactionList = TransactionList[['symbol','side','cumulative_quantity','average_price','fees','timestamp']] \
+                        .rename(columns={'side':'action','cumulative_quantity':'shares', \
+                                         'timestamp':'date', 'fees':'commission'})
+
+##convert date to datetime for sorting
+#TransactionList.date = pd.to_datetime(TransactionList.date, format = '%Y-%m-%dT%H:%M:%S')
+#
+##sort by date with most recent at the top
+#TransactionList = TransactionList.sort_values('date', axis = 0, ascending= False).reset_index(drop= True)
+
+#If UpdateTransaction is False, CleanedMasterTransactionList needs to be created
+if UpdateTransaction == False:
+    
+    # save the CSV
+    try:
+        TransactionList.to_csv(CleanedMasterTransactionFilename)
+    except IOError:
+        print("Oops.  Unable to write file to ", CleanedMasterTransactionFilename)
+    
+#    if args.profit:
+#        profit_csv = profit_extractor(csv, CleanedMasterTransactionFilename)
+
+
+#If MasterTransactionExists is True, UpdateTransactionList needs to be created and appended to MasterTransactionList
 else:
-    print("No trade history found in your account.")
-    quit()
+    
+    #Save UpdateTransactionList as .csv
+    TransactionList.to_csv(UpdateTransactionFilename) 
+     
+    #Appends MasterTransactionList which contains updates to top of CleanedMasterTransactionList
+    pd.concat([pd.read_csv(CleanedMasterTransactionFilename, index_col=0), TransactionList], ignore_index = True) \
+                .to_csv(CleanedMasterTransactionFilename) 
 
-# CSV headers
-keys = fields[0].keys()
-keys = sorted(keys)
-csv = ','.join(keys) + "\n"
+#Saves LastTransaction for future use
+LastTransaction.to_csv(LastTransactionFilename)
 
-# CSV rows
-for row in fields:
-    for idx, key in enumerate(keys):
-        if (idx > 0):
-            csv += ","
-        try:
-            csv += str(fields[row][key])
-        except:
-            csv += ""
-
-    csv += "\n"
-
-# choose a filename to save to
-print("Choose a filename or press enter to save to `robinhood.csv`:")
+#prompt user there were no updates
+print("Export completed, press enter to exit.", end=' ')
 try:
     input = raw_input
 except NameError:
     pass
-filename = input().strip()
-if filename == '':
-    filename = "robinhood.csv"
-
-# save the CSV
-try:
-    with open(filename, "w+") as outfile:
-        outfile.write(csv)
-except IOError:
-    print("Oops.  Unable to write file to ", filename)
-
-if args.profit:
-    profit_csv = profit_extractor(csv, filename)
+input()
